7c3bc0d595ce043a127e1bece3d7efa2909eae5d
jyw-atgithub <jenyuw@uci.edu>
2024-01-28 01:46:24 -0800

    try models

---
+++ 10_badread_profile.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+#SBATCH --job-name=profile
+#SBATCH -A jje_lab
+#SBATCH -p standard
+#SBATCH --array=1
+#SBATCH --cpus-per-task=30
+#SBATCH --mem-per-cpu=6G
+
+ref="/dfs7/jje/jenyuw/Eval-sv-temp/reference"
+sim="/dfs7/jje/jenyuw/Eval-sv-temp/simulation"
+nT=$SLURM_CPUS_PER_TASK
+
+module load python/3.8.0
+
+module unload python/3.8.0
--- 12_visor_reads.sh
+++ 12_visor_reads.sh
@@ -3,27 +3,38 @@
 #SBATCH --job-name=reads
 #SBATCH -A jje_lab
 #SBATCH -p standard
-#SBATCH --array=1-10
-#SBATCH --cpus-per-task=30
+#SBATCH --array=1-13
+#SBATCH --cpus-per-task=32
 #SBATCH --mem-per-cpu=6G
 
 ref="/dfs7/jje/jenyuw/Eval-sv-temp/reference"
 sim="/dfs7/jje/jenyuw/Eval-sv-temp/simulation"
 nT=$SLURM_CPUS_PER_TASK
 
-cd $sim
-module load R/4.2.2
 module load python/3.8.0
 
-coverage=`expr $SLURM_ARRAY_TASK_ID \* 10`
-echo "coverage is $coverage \%"
+parameter=`head -n $SLURM_ARRAY_TASK_ID ${sim}/parameter.tsv | tail -n 1`
+model=`echo $parameter | gawk '{print $1}'`
+coverage=`echo $parameter | gawk '{print $2}'`
+l_mean=`echo $parameter | gawk '{print $3}'`
+l_stdev=`echo $parameter | gawk '{print $4}'`
 
-#read_type="nanopore" or "pacbio"
-VISOR LASeR -g ${ref}/r649.rename.fasta -s ${sim}/hap_1 -b ${sim}/reads.simple.bed -o CLR_${coverage}_1 \
+#coverage=`expr $SLURM_ARRAY_TASK_ID \* 10`
+echo "model is $model"
+echo "coverage is $coverage %"
+
+if [[ ${model} != "Hifi" ]]
+then
+###This setting is good for *noisy* reads. NOT for Hifi reads.
+VISOR LASeR -g ${ref}/r649.rename.fasta -s ${sim}/hap_1 -b ${sim}/reads.simple.bed -o ${model}_${coverage}_1 \
 --tag --fastq --compress \
 --threads ${nT} \
 --coverage ${coverage}  --read_type pacbio \
---length_mean 8500 --length_stdev 6000 \
---identity_min 90 --identity_max 96 --identity_stdev 3 \
---error_model "pacbio2016" --qscore_model "pacbio2016" --junk_reads 0.02 \
---glitches_rate 1000 --glitches_size 25 --glitches_skip 25
+--length_mean ${l_mean} --length_stdev ${l_stdev} \
+--identity_min 90 --identity_max 95 --identity_stdev 4 \
+--error_model ${model} --qscore_model ${model} --junk_reads 0.02 \
+--glitches_rate 10000 --glitches_size 25 --glitches_skip 25
+#glitches_rate 越小代表越糟糕
+fi
+
+module unload python/3.8.0
